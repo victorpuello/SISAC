e30a5498951a8c543466614471a94437b2868f71
thekingtork <victor.puello@gmail.com>
2018-11-06 22:03:59 -0500

    Fix DB String Lenght

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace ATS\Providers;
 
 use ATS\Model\Planilla;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+    	Schema::defaultStringLength(120);
         Blade::if('load', function (Planilla $planilla){
             return $planilla->isLoad();
         });
